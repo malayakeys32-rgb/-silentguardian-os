301c491e46d826efe39451c58abcf6c5dd370794
malayakeys32-rgb <malayakeys32@gmail.com>
2026-08-25 10:17:51 +0000

    Fix layout component import

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,11 @@
 import "../styles/hologram.css";
-import HoloFrame from "../components/HoloFrame";
+import HoloFrame from "../components/HaloFrame";
 
 export const metadata = {
   title: "Silent Guardian",
 };
 
-export default function RootLayout({ children }) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body className="sg-body">
